3e009f3a0dd9a6750f3a6c5b9e4c59d2ad13c8f0
Mike Dewar <md@bit.ly>
2012-01-16 11:33:22 -0500

    refactored and pep8'd the css object

--- css.py
+++ css.py
@@ -1,37 +1,51 @@
 #!/usr/bin/python
 
+
 class CSS:
-	def __init__(self):
-		self.objects = {}
+    """
+    a CSS object is a dictionary whose keys are CSS selectors and whose values
+    are dictionaries of CSS declarations. This object is named according to the
+    definition of CSS on wikipedia:
+        A style sheet consists of a list of rules.
+        Each rule or rule-set consists of one or more selectors and a
+        declaration block.
+        A declaration-block consists of a list of declarations in braces.
+        Each declaration itself consists of a property, a colon (:), a value.
+    """
+    def __init__(self):
+        self.rules = {}
 
-	def __getitem__(self, item):
-		return self.objects[item]
+    def __getitem__(self, selector):
+        "returns the dictionary of CSS declarations, given a selector"
+        return self.rules[selector]
 
-	def __setitem__(self, item, value):
-		assert(isinstance(value, dict))
-		if item in self.objects:
-			self.objects[item].update(value)
-		else:
-			self.objects[item] = value
+    def __setitem__(self, selector, declarations):
+        "adds a dictionary of CSS declarations to the specified selector"
+        assert(isinstance(declarations, dict))
+        if selector in self.rules:
+            self.rules[selector].update(declarations)
+        else:
+            self.rules[selector] = declarations
 
-	def __add__(self, item):
-		if isinstance(item, dict):
-			for value in item.itervalues():
-				assert(isinstance(item, dict))
-				self.objects.update(item)
-			return self
-		elif isinstance(item, CSS):
-			return self.__add__(item.objects)
-		else:
-			raise Exception("Unsupported addition between %s and %s"%(type(CSS), type(item)))
+    def __add__(self, css):
+        if isinstance(css, dict):
+            for selector, declarations in css.iteritems():
+                assert(isinstance(declarations, dict))
+                self.rules[selector] = declarations
+            return self
+        elif isinstance(css, CSS):
+            return self.__add__(css.rules)
+        else:
+            errmsg = "Unsupported addition between %s and %s"
+            raise Exception(errmsg % (type(self), type(css)))
 
-	def __str__(self):
-		css = ""
-		for key, props in self.objects.iteritems():
-			css += "%s {\n"%key
-			for prop, value in props.iteritems():
-				if value is None:
-					value = "none"
-				css += "\t%s: %s;\n"%(prop, value)
-			css += "}\n\n"
-		return css
+    def __str__(self):
+        css = ""
+        for selector, declarations in self.rules.iteritems():
+            css += "%s {\n" % selector
+            for prop, value in declarations.iteritems():
+                if value is None:
+                    value = "none"
+                css += "\t%s: %s;\n" % (prop, value)
+            css += "}\n\n"
+        return css
