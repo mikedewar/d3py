6fc2828968bc62a4e8a184507494ee170f6ac9d5
Mike Dewar <mikedewar@gmail.com>
2012-05-07 06:21:25 -0400

    removed axis geom import

--- d3py/geoms/__init__.py
+++ d3py/geoms/__init__.py
@@ -2,8 +2,8 @@
 
 from xaxis import xAxis
 from yaxis import yAxis
-from axis import Axis
+#from axis import Axis
 from point import Point
 from bar import Bar
 from line import Line
-
+from geom import Geom
