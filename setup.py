82d6519ca49ee18763cf13e3ac8d83d2c66af9f4
Mike Dewar <mikedewar@gmail.com>
2012-12-27 14:48:08 -0500

    added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@
 
 from distutils.core import setup
 
-setup(name='d3py',
-      version='0.2.3',
-      description='d3py',
-      author='Mike Dewar, Micha Gorelick and Adam Laiacano',
-      author_email='md@bit.ly',
-      url='https://github.com/mikedewar/D3py',
-      packages=['d3py', 'd3py.geoms', ],
-      package_data={'d3py': ['d3.js','d3py_template.html']},
- )
+setup(
+    name='d3py',
+    version='0.2.3',
+    description='d3py',
+    author='Mike Dewar, Micha Gorelick and Adam Laiacano',
+    author_email='md@bit.ly',
+    url='https://github.com/mikedewar/D3py',
+    packages=['d3py', 'd3py.geoms', ],
+    package_data={'d3py': ['d3.js','d3py_template.html']},
+    requires=['pandas','networkx']
+)
