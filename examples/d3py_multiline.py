a63ef75ab04f300a9d2144a8e97a0990c21aeda5
Mike Dewar <mikedewar@gmail.com>
2013-02-10 12:09:15 -0500

    removed some commented out lines from the examples

--- examples/d3py_multiline.py
+++ examples/d3py_multiline.py
@@ -17,7 +17,6 @@ df = pandas.DataFrame({
 with d3py.PandasFigure(df, 'd3py_line', width=600, height=200) as fig:
     fig += d3py.geoms.Line('x', 'y', stroke='BlueViolet')
     fig += d3py.geoms.Line('x', 'z', stroke='DeepPink')
-    #fig += d3py.geoms.Point('x', 'y', fill='BlueViolet', opacity=0.9)
     fig += d3py.xAxis('x')
     fig += d3py.yAxis('y')
     fig.show()
