eff593ce3bc0e35e4b6917e396c203f9c0c716d5
Mike Dewar <mikedewar@gmail.com>
2013-02-10 12:06:49 -0500

    added an example with two lines

---
+++ examples/d3py_multiline.py
@@ -0,0 +1,23 @@
+import numpy as np
+import d3py
+import pandas
+
+T = 5*np.pi
+x = np.linspace(-T,T,100)
+a = 0.05
+y = np.exp(-a*x) * np.sin(x)
+z = np.exp(-a*x) * np.sin(0.5*x)
+
+df = pandas.DataFrame({
+    'x' : x,
+    'y' : y,
+    'z' : z,
+})
+
+with d3py.PandasFigure(df, 'd3py_line', width=600, height=200) as fig:
+    fig += d3py.geoms.Line('x', 'y', stroke='BlueViolet')
+    fig += d3py.geoms.Line('x', 'z', stroke='DeepPink')
+    #fig += d3py.geoms.Point('x', 'y', fill='BlueViolet', opacity=0.9)
+    fig += d3py.xAxis('x')
+    fig += d3py.yAxis('y')
+    fig.show()
