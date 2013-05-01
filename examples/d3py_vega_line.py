3f1f659ab5759975262edf7719840f6c6956fc89
Mike Dewar <michael.dewar@nytimes.com>
2013-05-01 16:49:41 -0400

    started rationalising examples to be stand alone (aka use the with idiom)

--- examples/d3py_vega_line.py
+++ examples/d3py_vega_line.py
@@ -8,11 +8,11 @@ y = [random.randint(10, 100) for num in range(0, 101, 1)]
 df = pd.DataFrame({'x': x, 'y': y})
 
 #Create Pandas figure
-fig = d3py.PandasFigure(df, 'd3py_area', port=8000, columns=['x', 'y'])
+with d3py.PandasFigure(df, 'd3py_vega_line', port=8000, columns=['x', 'y']) as fig:
 
-#Add Vega Area plot
-fig += d3py.vega.Line()
+    #Add Vega Area plot
+    fig += d3py.vega.Line()
 
-#Show figure
-fig.show()
+    #Show figure
+    fig.show()
 
